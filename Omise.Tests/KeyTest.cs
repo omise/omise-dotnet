d0fd6181ce2d199a8d831cb0cdc6411ce1238bfe
Chakrit Wichian <service@chakrit.net>
2015-12-03 10:34:28 +0700

    Key type.

---
+++ Omise.Tests/KeyTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Omise;
+
+namespace Omise.Tests {
+    [TestFixture]
+    public class KeyTest {
+        [Test]
+        public void TestStringImplicitAssignable() {
+            var s = "pkey_string_test";
+            Key key = s;
+
+            Assert.AreEqual(s, (string)key);
+        }
+
+        [Test]
+        public void TestModes() {
+            Key pkey = "pkey_test_123";
+            Key skey = "skey_123";
+
+            Assert.IsTrue(pkey.IsTest);
+            Assert.IsTrue(skey.IsLive);
+        }
+
+        [Test]
+        public void TestToString() {
+            var s = "pkey_string_test";
+            Key k = s;
+
+            Assert.AreSame(k.ToString(), s);
+        }
+    }
+}
+
---
+++ Omise/Key.cs
@@ -0,0 +1,21 @@
+﻿namespace Omise {
+    struct Key {
+        string value;
+
+        public bool IsTest { get { return value.Contains("_test_"); } }
+        public bool IsLive { get { return !IsTest; } }
+
+        public static implicit operator Key(string value) {
+            return new Key { value = value };
+        }
+
+        public static implicit operator string(Key key) {
+            return key.value;
+        }
+
+        public override string ToString() {
+            return this;
+        }
+    }
+}
+
