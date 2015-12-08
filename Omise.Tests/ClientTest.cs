7ef2007599757a33dc5b5b636b73367581e2d870
Chakrit Wichian <service@chakrit.net>
2015-12-08 16:58:01 +0700

    Initial Client interface.

---
+++ Omise.Tests/ClientTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using Omise.Models;
+
+namespace Omise.Tests {
+    [TestFixture]
+    public class ClientTest : OmiseTest {
+        [Test]
+        public void TestCtor() {
+            var pkey = "pkey_test_123";
+            var skey = "skey_test_123";
+
+            Assert.Throws<ArgumentException>(() => new Client(null, null));
+            Assert.DoesNotThrow(() => new Client(pkey, null));
+            Assert.DoesNotThrow(() => new Client(null, skey));
+            Assert.DoesNotThrow(() => new Client(pkey, skey));
+
+            var creds = DummyCredentials;
+            Assert.Throws<ArgumentNullException>(() => new Client(credentials: null));
+            Assert.DoesNotThrow(() => new Client(creds));
+        }
+
+        [Test]
+        public void TestResources() {
+            var client = new Client(DummyCredentials);
+            var resources = new object[]
+            {
+                client.Account,
+                client.Balance,
+                client.Charges,
+                client.Customers,
+                client.Disputes,
+                client.Events,
+                client.Recipients,
+                client.Tokens,
+                client.Transactions,
+                client.Transfers,
+            };
+
+            foreach (var resource in resources) {
+                Assert.IsNotNull(resource);
+            }
+        }
+    }
+}
+
---
+++ Omise/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Omise;
+using Omise.Models;
+using Omise.Resources;
+
+namespace Omise {
+    public class Client {
+        IRequester Requester { get; set; }
+
+        public readonly AccountResource Account;
+        public readonly BalanceResource Balance;
+        //card
+        public readonly ChargeResource Charges;
+        public readonly CustomerResource Customers;
+        public readonly DisputeResource Disputes;
+        public readonly EventResource Events;
+        public readonly RecipientResource Recipients;
+        //refunds
+        public readonly TokenResource Tokens;
+        public readonly TransactionResource Transactions;
+        public readonly TransferResource Transfers;
+
+        
+        public Client(string pkey = null, string skey = null)
+            : this(new Credentials(pkey, skey)) {
+        }
+
+        public Client(Credentials credentials) {
+            if (credentials == null) throw new ArgumentNullException("credentials");
+
+            Requester = new Requester(credentials);
+
+            Account = new AccountResource(Requester);
+            Balance = new BalanceResource(Requester);
+            Charges = new ChargeResource(Requester);
+            Customers = new CustomerResource(Requester);
+            Disputes = new DisputeResource(Requester);
+            Events = new EventResource(Requester);
+            Recipients = new RecipientResource(Requester);
+            Tokens = new TokenResource(Requester);
+            Transactions = new TransactionResource(Requester);
+            Transfers = new TransferResource(Requester);
+        }
+    }
+}
