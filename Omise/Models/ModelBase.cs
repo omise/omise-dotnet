5f85e250a71cdb9176d842011fc695c67f1ecdb3
Chakrit Wichian <service@chakrit.net>
2015-12-09 16:29:42 +0700

    Rewrite model template + Add Equals and GetHashCode() impl.

--- Omise/Models/ModelBase.cs
+++ Omise/Models/ModelBase.cs
@@ -25,6 +25,32 @@ namespace Omise.Models {
         public bool Deleted { get; set; }
 
         // TODO: Provide Task<T> Reload() functionality.
+
+        public override bool Equals(object obj) {
+            if (obj == null) return false;
+            var another = (ModelBase)obj;
+
+            return this.Object == another.Object &&
+            this.Id == another.Id &&
+            this.LiveMode == another.LiveMode &&
+            this.Location == another.Location &&
+            this.Created == another.Created &&
+            this.Deleted == another.Deleted;
+        }
+
+        public override int GetHashCode() {
+            unchecked {
+                int hash = 17;
+                hash = hash * 23 + Object.GetHashCode();
+                hash = hash * 23 + Id.GetHashCode();
+                hash = hash * 23 + LiveMode.GetHashCode();
+                hash = hash * 23 + Location.GetHashCode();
+                hash = hash * 23 + Created.GetHashCode();
+                hash = hash * 23 + Deleted.GetHashCode();
+
+                return hash;
+            }
+        }
     }
 }
 
