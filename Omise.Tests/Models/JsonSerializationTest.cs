24028a4d91bb14b64e53ab1890efe3909edd3146
Chakrit Wichian <service@chakrit.net>
2015-12-11 11:17:02 +0700

    Include fixtures in generated test data.

--- Omise.Tests/Models/JsonSerializationTest.cs
+++ Omise.Tests/Models/JsonSerializationTest.cs
@@ -10,28 +10,27 @@ using System.IO;
 namespace Omise.Tests.Models {
     [TestFixture]
     public class JsonSerializationTest : OmiseTest {
-        static readonly IDictionary<byte[], Type> mappings = new Dictionary<byte[], Type>
-        {
-            { TestData.Objects.account_object_json, typeof(Account) },
-            { TestData.Objects.balance_object_json, typeof(Balance) },
-            { TestData.Objects.bank_account_object_json, typeof(BankAccount) },
-            { TestData.Objects.card_object_json, typeof(Card) },
-            { TestData.Objects.charge_object_json, typeof(Charge) },
-            { TestData.Objects.customer_object_json, typeof(Customer) },
-            { TestData.Objects.dispute_object_json, typeof(Dispute) },
-            { TestData.Objects.event_object_json, typeof(Event) },
-            { TestData.Objects.recipient_object_json, typeof(Recipient) },
-            { TestData.Objects.refund_object_json, typeof(Refund) },
-            { TestData.Objects.token_object_json, typeof(Token) },
-            { TestData.Objects.transaction_object_json, typeof(Transaction) },
-            { TestData.Objects.transfer_object_json, typeof(Transfer) },
-        };
-
+        static readonly Type[] modelTypes =
+            {
+                typeof(Account),
+                typeof(Balance),
+                typeof(BankAccount),
+                typeof(Card),
+                typeof(Charge),
+                typeof(Customer),
+                typeof(Dispute),
+                typeof(Event),
+                typeof(Recipient),
+                typeof(Refund),
+                typeof(Token),
+                typeof(Transaction),
+                typeof(Transfer)
+            };
+        
         [Test]
         public void TestJsonSerialize() {
             var serializer = new Serializer();
-            foreach (var pair in mappings) {
-                var type = pair.Value;
+            foreach (var type in modelTypes) {
                 var method = serializer.GetType().GetMethod("JsonSerialize");
                 method = method.MakeGenericMethod(type);
 
@@ -39,28 +38,29 @@ namespace Omise.Tests.Models {
                     var instance = Activator.CreateInstance(type);
                     method.Invoke(serializer, new object[] { ms, instance });
                 }
-
-                // TODO: Actually test serialized data.
             }
         }
 
         [Test]
         public void TestJsonDeserialize() {
             var serializer = new Serializer();
-            foreach (var pair in mappings) {
-                var bytes = pair.Key;
-                var type = pair.Value;
-
+            foreach (var type in modelTypes) {
                 var method = serializer.GetType().GetMethod("JsonDeserialize");
                 method = method.MakeGenericMethod(type);
+               
+                var filename = "objects/" + ModelTypes.NameFor(type) + "_object.json";
+                if (!TestData.Files.ContainsKey(filename)) {
+                    throw new Exception(filename);
+                }
 
-                object result;   
-                using (var ms = new MemoryStream(bytes)) {
+                var filedata = TestData.Files[filename];
+
+                object result;
+                using (var ms = new MemoryStream(filedata)) {
                     result = method.Invoke(serializer, new object[]{ ms });
                 }
 
                 Assert.IsInstanceOf(type, result);
-                // TODO: Actually assert that correct data is serialized.
             }
         }
     }
