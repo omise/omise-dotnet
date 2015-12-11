4aba71db0b7ef2dd0c46843ba5f893d5ecb7ff74
Chakrit Wichian <service@chakrit.net>
2015-12-11 11:17:18 +0700

    Test fixes.

--- Omise/Models/Event.cs
+++ Omise/Models/Event.cs
@@ -1,18 +1,9 @@
 ﻿using System;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Linq;
-using System.IO;
-using System.Text;
 
 namespace Omise.Models {
-    public class Event : ModelBase {
+    public partial class Event : ModelBase {
         ModelBase data;
-
-        public string Key { get; set; }
-
-        [JsonProperty("data")]
-        public JRaw RawDataJson { get; set; }
 
         [JsonIgnore]
         public ModelBase Data {
@@ -28,33 +19,6 @@ namespace Omise.Models {
             var json = (string)RawDataJson.Value;
             serializer.JsonPopulate(json, instance);
             return instance;
-        }
-    }
-
-
-    // HACK: Workaround Newtonsoft.Json not giving us a choice to partially deserialize.
-    // REF: http://stackoverflow.com/a/11223607/3055
-    public class RawJsonConverter : JsonConverter {
-        public override bool CanRead { get { return false; } }
-        public override bool CanWrite { get { return false; } }
-
-        public override bool CanConvert(Type objectType) {
-            return true;
-        }
-
-        public override void WriteJson(
-            JsonWriter writer,
-            object value,
-            JsonSerializer serializer) {
-            throw new NotImplementedException();
-        }
-
-        public override object ReadJson(
-            JsonReader reader,
-            Type objectType,
-            object existingValue,
-            JsonSerializer serializer) {
-            return JObject.Load(reader).ToString();
         }
     }
 }
