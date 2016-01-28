34f1e6dd177dd8f90ec120206ab13b76ad10afca
Chakrit Wichian <service@chakrit.net>
2016-01-28 15:49:32 +0700

    v2.0.1.28461

--- Omise.Tests/AssemblyInfo.cs
+++ Omise.Tests/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.0.*")]
-[assembly: AssemblyInformationalVersion("2.0.0.*")]
+[assembly: AssemblyVersion("2.0.1.*")]
+[assembly: AssemblyInformationalVersion("2.0.1.*")]
