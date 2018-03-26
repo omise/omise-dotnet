a9c265725a8bdd4f4f8ffa8d5669282b660f152e
Tony Woodhouse <tony.woodhouse@omise.co>
2018-03-26 15:47:31 +0700

    Added support for refunds metadata.

--- Omise/Models/RefundRequest.cs
+++ Omise/Models/RefundRequest.cs
@@ -1,8 +1,11 @@
-﻿namespace Omise.Models
+﻿using System.Collections.Generic;
+
+namespace Omise.Models
 {
     public class CreateRefundRequest : Request
     {
         public long Amount { get; set; }
         public bool Void { get; set; }
+        public IDictionary<string, object> Metadata { get; set; }
     }
 }
