5e77a1c5a986948af23207693a9967a70e566f95
hpariwat <61586978+hpariwat@users.noreply.github.com>
2021-02-19 16:22:16 +0700

    Add mock test for capability

---
+++ Omise.Tests/Resources/CapabilityResourceTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using Omise.Resources;
+using System.Threading.Tasks;
+
+namespace Omise.Tests.Resources
+{
+    [TestFixture]
+    public class CapabilityResourceTest : ResourceTest<CapabilityResource>
+    {
+        [Test]
+        public async Task TestGet()
+        {
+            await Resource.Get();
+            AssertRequest("GET", "https://api.omise.co/capability");
+        }
+
+        [Test]
+        public async Task TestFixturesGet()
+        {
+            var capability = await Fixtures.Get();
+
+            Assert.AreEqual("TH", capability.Country);
+            Assert.False(capability.ZeroInterestInstallments);
+            Assert.That(capability.Banks, Does.Contain("kbank"));
+
+            var fpx = capability.PaymentMethods.Find(P => P.Name == "fpx");
+            var bank = fpx.Banks.Find(B => B.Code == "cimb");
+
+            Assert.AreEqual("fpx", fpx.Name);
+            Assert.Null(fpx.InstallmentTerms);
+
+            Assert.AreEqual("CIMB Clicks", bank.Name);
+            Assert.True(bank.Active);
+
+        }
+
+        protected override CapabilityResource BuildResource(IRequester requester)
+        {
+            return new CapabilityResource(requester);
+        }
+    }
+}
