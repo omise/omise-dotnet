933be99528b0671587ff2486129a8114361f2e16
hpariwat <61586978+hpariwat@users.noreply.github.com>
2021-02-19 14:51:13 +0700

    Add some usage examples

---
+++ Omise.Examples/Examples/Capability.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace Omise.Examples
+{
+    public class Capability : Example
+    {
+        public async Task Retrieve__Retrieve()
+        {
+            var capability = await Client.Capability.Get();
+            var banks = capability.Banks;
+            var paymentMethods = capability.PaymentMethods;
+
+            Console.WriteLine($"country: {capability.Country}");
+            Console.WriteLine($"zero interest installments: {capability.ZeroInterestInstallments}");
+
+            foreach (var bank in banks)
+            {
+                Console.WriteLine($"supported bank: {bank}");
+            }
+
+            foreach (var paymentMethod in paymentMethods)
+            {
+                Console.WriteLine($"supported payment method: {paymentMethod.Name}");
+            }
+        }
+    }
+}
