a0200fe2d984316303a772c90c036f405656b5b3
AnasNaouchi <anas.n@opn.ooo>
2023-08-21 10:38:22 +0300

    Change ExpireAt to nullable

--- Omise/Models/ChargeRequest.cs
+++ Omise/Models/ChargeRequest.cs
@@ -12,7 +12,7 @@ namespace Omise.Models
         public string Currency { get; set; }
         public string Description { get; set; }
         [JsonProperty("expires_at")]
-        public DateTime ExpiresAt { get; set; }
+        public DateTime? ExpiresAt { get; set; }
         public IDictionary<string, object> Metadata { get; set; }
         public bool Capture { get; set; }
         public OffsiteTypes Offsite { get; set; }
